1b5099e4c6b274527a20f52b071dbebdcb1fa90a
sonu <sonu444.karmacharya@gmail.com>
2020-06-11 08:16:31 +0545

    modified

--- routes/api.js
+++ routes/api.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const BlogPost = require("../models/blogPost");
 
 router.get("/", (req, res) => {
-  BlogPost.find({})
+  BlogPost.find()
+    .sort({ date: -1 })
     .then((data) => {
-      // console.log(data);
       res.json({ status: 200, lists: data });
     })
     .catch((err) => {
@@ -14,10 +14,24 @@ router.get("/", (req, res) => {
     });
 });
 
-router.route("/saves").post((req, res) => {
+router.get("/:id", (req, res) => {
+  BlogPost.findById(req.params.id, function (err, data) {
+    res.json({ status: 200, lists: data });
+  });
+});
+router.put("edit/:id", (req, res) => {
+  BlogPost.findByIdAndUpdate(req.params.id)
+    .then((data) => {
+      res.json({ status: 200, lists: data });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json({ status: 400, message: "Error", error: err });
+    });
+});
+router.post("/saves", (req, res) => {
   const data = req.body;
   const newBlogPost = new BlogPost(data);
-
   newBlogPost.save((err) => {
     if (err) {
       res.status(500).json({ msg: "Sorry somethg went wrong" });
@@ -27,11 +41,25 @@ router.route("/saves").post((req, res) => {
   });
 });
 
-router.get("/name", (req, res) => {
-  const data = {
-    username: "abc2",
-    age: 10,
-  };
-  res.json(data);
+router.delete("/:id", (req, res) => {
+  BlogPost.findById(req.params.id)
+    .then((data) =>
+      data.remove().then(() => res.json({ success: true, data: data }))
+    )
+    .catch((err) => res.json({ success: false }));
+});
+
+router.put("/edit/:id", (req, res) => {
+  BlogPost.findById(req.params.id)
+    .then((data) => {
+      data.title = req.body.title;
+      data.body = req.body.body;
+
+      data
+        .save()
+        .then(() => res.json("updated"))
+        .catch((err) => res.status(400).json(err));
+    })
+    .catch((err) => console.log(err));
 });
 module.exports = router;
